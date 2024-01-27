2c981bcd89f079702141388615cc2e26d6b16e0b
Sai0620 <sravan.moturi36@gmail.com>
2024-01-27 13:52:31 -0500

    day 2 if statement

---
+++ src/main/java/Day2/IfCondition.java
@@ -0,0 +1,14 @@
+package Day2;
+
+public class IfCondition {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+
+        if (a > b) {
+            System.out.println("false");
+        } else {
+            System.out.println("true");
+        }
+    }
+}
--- src/main/java/day1/SumOfTwoNum.java
+++ src/main/java/day1/SumOfTwoNum.java
@@ -2,9 +2,9 @@ package day1;
 
 public class SumOfTwoNum {
     public static void main(String[] args) {
-        int num1 = 10;
-        int num2 = 20;
-        int sum = num1 + num2;
+        double num1 = 1000.55;
+        double num2 = 2097.08;
+        double sum = num1 + num2;
         System.out.println("The sum of these is:" +sum);
 
     }

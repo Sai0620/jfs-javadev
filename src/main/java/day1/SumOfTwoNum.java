aa76a70db15ab1045a2e9a2ef4c7a8e4e07a94b7
Sai0620 <sravan.moturi36@gmail.com>
2024-01-26 19:23:34 -0500

    Initial commit

---
+++ src/main/java/day1/SumOfTwoNum.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class SumOfTwoNum {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+        System.out.println("The sum of these is:" +sum);
+
+    }
+}
